4276da5e16170e97eae20283799baf7b55a1d568
MacVibe <micahacos1000@gmail.com>
2026-03-31 02:08:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const { TextEncoder } = require("util");
 const WS_URL = "wss://ip-207-148-8-148.cavegame.io";
 const encoder = new TextEncoder();
 
+const MAX_OPEN_BOTS = 84;
+let openBots = 0;
+
 function buildIntroPacket() {
     const name = encoder.encode("\uD83D\uDD11" + Math.floor(Math.random() * 1000));
     const packet = new Uint8Array(3 + name.length);
@@ -14,22 +17,10 @@ function buildIntroPacket() {
     return packet;
 }
 
-const TEAM_JOIN_PACKET = Uint8Array.from([
-    49,31,47,116,101,97,109,32,106,111,105,110,32,
-    84,101,115,116,101,114,115,32,
-    103,51,56,57,56,101,110,97,107,108,49,48
-]);
-
-const TEAM_JOINED_PACKET = Uint8Array.from([
-    24,0,0,12,84,101,97,109,32,106,111,105,110,101,100,33,4,103,111,111,100
-]);
-
-const CHAT_JOIN_PACKET = Uint8Array.from([
-    49,10,47,116,101,97,109,32,99,104,97,116
-]);
-
+const TEAM_JOIN_PACKET = Uint8Array.from([49,31,47,116,101,97,109,32,106,111,105,110,32,84,101,115,116,101,114,115,32,103,51,56,57,56,101,110,97,107,108,49,48]);
+const TEAM_JOINED_PACKET = Uint8Array.from([24,0,0,12,84,101,97,109,32,106,111,105,110,101,100,33,4,103,111,111,100]);
+const CHAT_JOIN_PACKET = Uint8Array.from([49,10,47,116,101,97,109,32,99,104,97,116]);
 const INFINITE_PACKET = Uint8Array.from([49,120,0]);
-
 const HEARTBEATS = [
     Uint8Array.from([34,0,0,0,0,0,64,128,0,192,195,166,192,0]),
     Uint8Array.from([34,0,0,0,0,0,194,143,255,252,67,177,63,255])
@@ -50,9 +41,12 @@ function sendIntro(ws) {
 }
 
 function spawnBot() {
+    if (openBots >= MAX_OPEN_BOTS) return;
+    openBots++;
+
     const ws = new WebSocket(WS_URL);
-    let teamInterval;
-    let infiniteInterval;
+    let teamInterval = null;
+    let infiniteInterval = null;
     let joined = false;
 
     ws.on("open", () => {
@@ -60,6 +54,7 @@ function spawnBot() {
         teamInterval = setInterval(() => {
             if (ws.readyState === WebSocket.OPEN && !joined) ws.send(TEAM_JOIN_PACKET);
         }, 1000);
+
         let hbIdx = 0;
         ws._hb = setInterval(() => {
             if (ws.readyState === WebSocket.OPEN) ws.send(HEARTBEATS[hbIdx++ % 2]);
@@ -73,18 +68,21 @@ function spawnBot() {
             ws.send(CHAT_JOIN_PACKET);
             infiniteInterval = setInterval(() => {
                 if (ws.readyState === WebSocket.OPEN) ws.send(INFINITE_PACKET);
-            }, 10);
+            }, 40);
         }
     });
 
-    ws.on("close", () => {
+    function cleanupAndSpawnNew() {
         clearInterval(ws._hb);
         clearInterval(teamInterval);
         clearInterval(infiniteInterval);
-        spawnBot();
-    });
+        if (ws.readyState === WebSocket.OPEN || ws.readyState === WebSocket.CONNECTING) ws.terminate();
+        openBots--;
+        setTimeout(spawnBot, 0);
+    }
 
-    ws.on("error", () => ws.terminate());
+    ws.on("close", cleanupAndSpawnNew);
+    ws.on("error", cleanupAndSpawnNew);
 }
 
-spawnBot();
+for (let i = 0; i < MAX_OPEN_BOTS; i++) spawnBot();
