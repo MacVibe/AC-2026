e439324559a9405c8ac5ef4c0514f6951a026d1c
MacVibe <micahacos1000@gmail.com>
2026-03-31 01:31:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const WebSocket = require("ws");
-const { TextEncoder } = require("util");
+const { TextEncoder, TextDecoder } = require("util");
 
 const WS_URL = "wss://ip-207-148-8-148.cavegame.io";
 
 const encoder = new TextEncoder();
+const decoder = new TextDecoder();
 
 function buildIntroPacket() {
-    const name = encoder.encode(
-        "\uD83D\uDD11" + Math.floor(Math.random() * 1000),
-    );
+    const name = encoder.encode("\uD83D\uDD11" + Math.floor(Math.random() * 1000));
     const packet = new Uint8Array(3 + name.length);
     packet[0] = 31;
     packet[1] = 1;
@@ -28,35 +27,51 @@ const HEARTBEATS = [
     Uint8Array.from([34, 0, 0, 0, 0, 0, 194, 143, 255, 252, 67, 177, 63, 255]),
 ];
 
+const TEAM_JOIN_PACKET = Uint8Array.from([49,31,47,116,101,97,109,32,106,111,105,110,32,84,101,115,116,101,114,115,32,103,51,56,57,56,101,110,97,107,108,49,48]);
+const TEAM_CHAT_PACKET = Uint8Array.from([49,10,47,116,101,97,109,32,99,104,97,116]);
+const CHAT_LOOP_PACKET = Uint8Array.from([49,120,0]);
+
+function isTeamJoinedPacket(bytes) {
+    try {
+        return decoder.decode(bytes).includes("Team joined!");
+    } catch {
+        return false;
+    }
+}
+
 function spawnBot() {
     const ws = new WebSocket(WS_URL);
+    ws._joined = false;
 
     ws.on("open", () => {
         sendIntro(ws);
-
+        setTimeout(() => {
+            if (ws.readyState === WebSocket.OPEN) ws.send(TEAM_JOIN_PACKET);
+        }, 500);
         let hbIdx = 0;
         ws._hb = setInterval(() => {
-            if (ws.readyState === WebSocket.OPEN) {
-                ws.send(HEARTBEATS[hbIdx++ % 2]);
-            }
+            if (ws.readyState === WebSocket.OPEN) ws.send(HEARTBEATS[hbIdx++ % 2]);
         }, 1000);
+    });
+
+    ws.on("message", (data) => {
+        const bytes = new Uint8Array(data);
+        if (!ws._joined && isTeamJoinedPacket(bytes)) {
+            ws._joined = true;
+            ws.send(TEAM_CHAT_PACKET);
+            ws._chatLoop = setInterval(() => {
+                if (ws.readyState === WebSocket.OPEN) ws.send(CHAT_LOOP_PACKET);
+            }, 200);
+        }
     });
 
     ws.on("close", () => {
         clearInterval(ws._hb);
+        clearInterval(ws._chatLoop);
         spawnBot();
     });
 
-    ws.on("error", () => {
-        ws.terminate();
-    });
+    ws.on("error", () => ws.terminate());
 }
 
 setInterval(spawnBot, 5);
-
-// 👇 Fake HTTP server for Render
-const http = require("http");
-
-http.createServer((req, res) => {
-    res.end("OK");
-}).listen(process.env.PORT || 3000);
